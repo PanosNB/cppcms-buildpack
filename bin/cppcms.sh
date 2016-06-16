07dab607012958c8ab9b68ab55099016f47ac2d8
PanosNB <patrospanos@gmail.com>
2016-06-16 17:27:14 -0300

    Listing app contents

--- bin/cppcms.sh
+++ bin/cppcms.sh
@@ -7,6 +7,7 @@ function indent() {
 }
 
 echo "-----> injecting environment into config.js ..."
+ls -l
 cp cppcms.js cppcms.js.template
 cat cppcms.js.template | \
   jq ".service.port=${PORT} | .service.ip=\"0.0.0.0\" | .service.api=\"http\"" | \
